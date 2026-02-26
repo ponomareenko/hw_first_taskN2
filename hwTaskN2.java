70444121ab8aa86eea3d0d2c9845be1f32ae8dc0
ponomareenko <el.pochtadp@gmail.com>
2026-02-26 09:08:31 +0300

    Add files via upload

---
+++ hwTaskN2.java
@@ -0,0 +1,10 @@
+public class hwTaskN2 {
+    void main() {
+        int income = 44000;
+        int spending = 30000;
+
+        int balance = income - spending;
+
+        System.out.println("Итого (руб):\n" + balance);
+    }
+}
